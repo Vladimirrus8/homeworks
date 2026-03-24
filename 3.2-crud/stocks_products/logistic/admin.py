f2f4c0b57c85210c55dce341e59908babc036ff6
Vladimir Puzenko <vpuzenko1@gmail.com>
2026-03-25 00:43:00 +0500

    Внесли исправления CRUD в DRF

--- 3.2-crud/stocks_products/logistic/admin.py
+++ 3.2-crud/stocks_products/logistic/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from logistic.models import Product, Stock, StockProduct
 
-# Register your models here.
+
+class StockProductInline(admin.TabularInline):
+    model = StockProduct
+    extra = 1
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description']
+    search_fields = ['title', 'description']
+
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ['id', 'address']
+    search_fields = ['address']
+    inlines = [StockProductInline]
